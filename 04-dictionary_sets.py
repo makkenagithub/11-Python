bb609576ab972a49d7a79d38ebb9aadc3e2285a2
devopus-git <ushaleadcherukuri@gmail.com>
2025-06-02 07:58:18 +0530

    python basics

--- 04-dictionary_sets.py
+++ 04-dictionary_sets.py
@@ -24,5 +24,33 @@ clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefaul
 """
 print(d.keys())
 print(d.values())
-print(d.items())    # returs list of tuples
+print(d.items())    # returs list of tuples  # [('k', 123), ((1, 2, 3, 4), 123), ((1, 2, 3, 4, 5), ['1', '2', '3'])]
 
+# dictionary is a mutable data type.
+d['k'] = 456
+print(d)
+
+
+
+
+# set
+s = {'a', 'b', 'c', 'a'}
+print(s)    # o/p; {'b', 'a', 'c'}
+# set does not allow dupkicate values. Set does not follow order of values given in set
+# set is immutable data type. We cant change the values of set once we define it.
+
+s[0] = 'abc'    # TypeError: set does not support item assignment
+
+print(dir(s))
+"""
+'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 
+'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 
+'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
+"""
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)  #o/p: True
+print("banssanaa" in thisset)  #o/p: False
+print("banana" not in thisset)  #o/p: False
+
+# in or not in  operator can be used for list, set, tuples etc
